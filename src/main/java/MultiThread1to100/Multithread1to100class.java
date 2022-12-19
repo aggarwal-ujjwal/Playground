bb970e8574d2665660589b2fb2d7f9a693b1bb3e
Ujjwal Aggarwal <79924606+aggarwal-ujjwal@users.noreply.github.com>
2022-12-19 20:12:07 +0530

    Update Multithread1to100class.java
    Added for loop for testing

--- src/main/java/MultiThread1to100/Multithread1to100class.java
+++ src/main/java/MultiThread1to100/Multithread1to100class.java
@@ -12,5 +12,8 @@ public class Multithread1to100class implements Runnable{
     @Override
     public void run() {
         System.out.println(number);
+        for(int i=0 ; i<10 ; i++)
+            System.out.println(i);
+            
     }
 }
