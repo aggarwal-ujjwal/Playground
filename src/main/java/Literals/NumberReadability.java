6377f67ab6b6fc9a2bf0c6481c058e4422e7f6ec
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-03-17 19:22:08 +0530

    Increased readability by using _ in numbers

---
+++ src/main/java/Literals/NumberReadability.java
@@ -0,0 +1,14 @@
+package Literals;
+
+public class NumberReadability {
+    public static void main(String[] args) {
+        Integer num1 = 1000000;
+        Integer num2 = 1_000_000;
+        if(num2==num1)
+            System.out.println(num2);
+        else
+            System.out.println("Not equal");
+        System.out.println(num2+num1);
+
+    }
+}
