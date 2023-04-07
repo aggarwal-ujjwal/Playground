0eb97a4425ba52d530c31138b4b90d1848c4cada
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-04-07 14:28:22 +0530

    String NPE

---
+++ src/main/java/StringsNPE/StringsNPE.java
@@ -0,0 +1,17 @@
+package StringsNPE;
+
+public class StringsNPE {
+    public static void main(String[] args) {
+        String str1 = null;
+        String str2 = "123";
+
+        if (!str2.equals(str1)) {        // #1
+            str2 = null;                 // #2
+        }
+
+        str1 = str2;                     // #3
+        str2 = str1;                     // #4
+
+        System.out.println(str2);
+    }
+}
