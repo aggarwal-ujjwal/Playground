82faea8e229739ac6f450edfdccd25dd9ae47118
aggarwal-ujjwal <aggarwal.ujjwal.17@gmail.com>
2023-05-05 19:37:48 +0530

    CaesarCipher for two keys

--- src/main/java/CaesarCipher/CaesarCipher.java
+++ src/main/java/CaesarCipher/CaesarCipher.java
@@ -5,21 +5,32 @@ public class CaesarCipher {
     public static void main(String[] args) {
         String str = "Can you imagine life WITHOUT the internet AND computers in your pocket?";
         StringBuilder result = new StringBuilder();
-        int s = 15;
+        int s1 = 21 , s2 = 8, num=0;
 
         char[] ch = str.toCharArray();
         for (char c : ch) {
             // apply transformation to each character
             // Encrypt Uppercase letters
-            if (c >= 'A' && c <= 'Z') {
-                result.append((char) ((c + s - 65) % 26 + 65));
+            if (num%2 ==0){
+                if (c >= 'A' && c <= 'Z') {
+                    result.append((char) ((c + s1 - 65) % 26 + 65));
+                }
+                // Encrypt Lowercase letters
+                else if (c >= 'a' && c <= 'z') {
+                    result.append((char) ((c + s1 - 97) % 26 + 97));
+                } else
+                    result.append(c);
+            } else {
+                if (c >= 'A' && c <= 'Z') {
+                    result.append((char) ((c + s2 - 65) % 26 + 65));
+                }
+                // Encrypt Lowercase letters
+                else if (c >= 'a' && c <= 'z') {
+                    result.append((char) ((c + s2 - 97) % 26 + 97));
+                } else
+                    result.append(c);
             }
-            // Encrypt Lowercase letters
-		    else if (c >= 'a' && c <= 'z') {
-                result.append((char) ((c + s - 97) % 26 + 97));
-            }
-            else
-                result.append(c);
+            num++;
         }
 
         System.out.println(result);
